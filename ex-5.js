a2fc0c15c23161b3f45fa7cf0c6ce8ea27b16baa
tum <hongsachm66.gmail.com>
2023-09-17 20:34:40 +0700

    tum's work

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalPrice = 27000;
+let discontPrice = totalPrice * 0.25;
+let checkoutPrice = totalPrice - discontPrice;
+console.log(checkoutPrice)
